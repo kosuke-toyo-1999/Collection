afdb92dc7f5bd42f13984ed94f0828e541a23f40
kosuke toyo <107592609+kosuke-toyo-1999@users.noreply.github.com>
2022-08-20 14:43:13 +0900

    新規Javaプロジェクト作成

---
+++ src/practice/Task.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+	
+	public Task()  {
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate d1 = LocalDate.parse("2021-10-21", f);
+		LocalDate d2 = LocalDate.parse("2021-9-15", f);
+		LocalDate d3 = LocalDate.parse("2021-12-4", f);
+		LocalDate d4 = LocalDate.parse("2021-8-10", f);
+		LocalDate d5 = LocalDate.parse("2021-11-9", f);
+		
+		System.out.println(d1);
+		
+	}
+	
+	
+
+}
