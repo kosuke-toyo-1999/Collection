c03d2b37fdf5fc8d1e3853e0681c15f91b27eea5
kosuke toyo <107592609+kosuke-toyo-1999@users.noreply.github.com>
2022-08-21 09:04:59 +0900

    更新

--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,8 +1,5 @@
 package practice;
 
-import java.util.Comparator;
-
-
 public class Task implements Comparable<Task> {
 	private int date;
 	private String task;
@@ -11,12 +8,12 @@ public class Task implements Comparable<Task> {
 		this.date = i;
 		this.task = task;
 	}
-}
+
 	// compareToメソッドをオーバーライドする
 	// 日付で並び替えるため、LocalDateのcompareToを使う
 	
-	static class Date implements Comparator<Task> {
-	public int compareTo(int t,Task a){
+	
+	public int compareTo(Task t,Task a){
 		
 		return t.date.compareTo(a.date);
 				//比較する内容を実装してください;
